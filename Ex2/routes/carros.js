9644c35ef3bd7ae9af7ce8fffe248e459d4b0e3a
lukasribeiro <luckas.ribeiro2015@gmail.com>
2019-02-16 11:36:03 -0200

    organizando rotas em arquivos separados

--- Ex2/routes/carros.js
+++ Ex2/routes/carros.js
@@ -1,64 +1,64 @@
 
-var express = require('express');
-var app = express.Router();
+let express = require('express');
+const router = express.Router();
 const CarroDB = require('../Model/CarrosBD');
 
-
 // GET em /carros
-Router.get('/carros', function (req, res) {
+router.get('/carros', function (req, res) {
 	CarroDB.getCarros(function(carros) {
 		res.json(carros)
 	});
-})
+});
 
 
 // GET em /carros/id
-Router.get('/carros/:id(\\d+)', function (req, res) {
+router.get('/carros/:id(\\d+)', function (req, res) {
 	let id = req.params.id;
 	CarroDB.getCarroById(id, function(carro) {
 		res.json(carro)
 	});
-})
+});
 
 
 // DELETE em /carros/id
-Router.delete('/carros/:id(\\d+)', function (req, res) {
+router.delete('/carros/:id(\\d+)', function (req, res) {
 	let id = req.params.id;
 	console.log("deletar arro " + id);
 	CarroDB.deleteById(id, function(affectedRows) {
 		res.json ({ msg: 'Carro deletado com sucesso.' })
 	});
-})
+});
 
 
-// GET em /carros/tipo (classicos, esportivos, luxo)
-Router.get('/carros/:tipo', function (req, res) {
+
+// GET em /carros/xxx
+router.get('/carros/:tipo', function (req, res) {
 	let tipo = req.params.tipo;
 	CarroDB.getCarrosByTipo(tipo, function(carros) {
 		res.json(carros)
 	});
-})
+});
+
 
 
 // POST para salvar um carro
-Router.post('/carros', function (req, res) {
+router.post('/carros', function (req, res) {
 	// Carro enviado no formato JSON
 	let carro = req.body;
 	CarroDB.save(carro, function(carro) {
 		res.json(carro)
 	});
-})
+});
+
 
 
 // PUT para atualizar um carro
-Router.put('/carros', function (req, res) {
+router.put('/carros', function (req, res) {
 	// Carro enviado no formato JSON
 	let carro = req.body;
 	CarroDB.update(carro, function(carro) {
-		// res.json(carro)
 		res.json ({ msg: 'Carro atualizado com sucesso.' })
 	});
-})
+});
 
-//exportar as rotas para usar em um outro arquivo
-module.exports = Router();
+module.exports  = router;
