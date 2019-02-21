0e258663de7c0440a1725c0e22957ad0daea7315
lukasribeiro <luckas.ribeiro2015@gmail.com>
2019-02-21 10:14:50 -0300

    Adicionando tratamento de erros!

--- capitulo11 - middleware e tratamento de erro/app.js
+++ capitulo11 - middleware e tratamento de erro/app.js
@@ -1,54 +1,59 @@
-let express = require('express');
-let app = express();
-let bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
 // Configura para ler dados do POST por form-urlencoded e application/json
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-/*
+
 // middleware  para interceptar a request e mostrar a data atual
 app.use(function (req, res, next) {
-    console.log('Time:', Date.now());
-    next();
-  });
-*/
+  console.log('Time:', Date.now());
+  next();
+});
+
+
+// Rota na raiz.
+app.get('/', function (req, res) {
+	res.send("API dos Carros");
+});
+
 
 // Rotas
-app.use('/api/carros', require('./routes/carros'));
+app.use('/api', require('./routes/carros'));
 
-
-//Tratamento de rotas para erros
-app.get('/teste_erro', function(req,res){
-    throw Error ('erro ninja muito loko');
+// Teste de Erro
+app.get('/teste_erro', function (req, res) {
+	throw Error('Erro Ninja');
 });
 
 
-//tratamento de erros 404
-app.use(function(err, req, res, next){
-    res.status(404);
-    res.json({err:'caminho nao encontrado!'});
+// Rota para não encontrado '404'
+app.use(function(req, res, next) {
+  res.status(404)
+  res.json({ err: "Não encontrado." });
 });
 
-/*
-// Rota para não encontrado '404' metodo diferente se de fazer em relação ao de cima
+
+// Rota para não encontrado '404'
 app.use(function logErrors(err, req, res, next) {
-    console.error(err.stack);
-    next(err);
-  });
-*/
-
-//rota generica de erro 500
-app.use(function(err, req, res, next){
-    console.log(err.stack);
-    res.status(500);
-    res.json({err: 'erro de transação! '});
-
+  console.error(err.stack);
+  next(err);
 });
+
+
+// Rota genérica de erro '500'
+app.use(function(err, req, res, next) {
+    //console.log(err)
+	res.status(500);
+  	res.json({ erro: 'Erro na transação' });
+});
+
 
 // Inicia o servidor
-let server = app.listen(3000, function () {
-    let host = server.address().address
-    let port = server.address().port
-    console.log("Servidor iniciado em http://%s:%s", host, port)
-});
+var server = app.listen(3000, function () {
+	var host = server.address().address
+	var port = server.address().port
+	console.log("Servidor iniciado em http://%s:%s", host, port)
+})
